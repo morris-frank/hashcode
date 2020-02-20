e3244c6129a6bcfd082a64c18626f830a54f5ed5
Maurice Frank <maurice.frank@posteo.de>
2020-02-20 22:05:21 +0100

    changed  fitn

--- fitness.py
+++ fitness.py
@@ -1,38 +1,12 @@
-from collections import defaultdict
-
-
 def fitness(L, books, D, B_scores, L_signuptimes, L_shipperday):
     assert len(books) == len(L)
-    scanned_books = set()
-
-    i_L = defaultdict(int)
 
     score = 0.
-    waiting = False
-    setupp = 0
-    active_libs = []
-    il = 0
-    for d in range(D):
-        if not waiting:
-            if il < len(L):
-                weiting_l = L[il]
-                setupp = L_signuptimes[weiting_l]
-                il += 1
-                waiting = True
-        for lib in active_libs:
-            for _ in range(L_shipperday[lib]):
-                if i_L[lib] < len(books[lib]) - 1:
-                    book = books[lib][i_L[lib]]
-                    if book not in scanned_books:
-                        scanned_books.add(book)
-                        score += B_scores[book]
-                    i_L[lib] += 1
-        if waiting:
-            setupp -= 1
-            if setupp == 0:
-                active_libs.append(weiting_l)
-                waiting = False
+
+    d = 0
+    for l in L:
+        d += L_signuptimes[l]
+        number_of_books = (D - d) * L_shipperday[l]
+        score += sum(books[l][:number_of_books])
 
     return score
-
-
