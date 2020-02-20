60dcb920918dd3f8d35491090b4dee266bc729ac
Maurice Frank <maurice.frank@posteo.de>
2020-02-20 20:30:40 +0100

    added save

--- saving.py
+++ saving.py
@@ -1,16 +1,15 @@
 # book key: library id; value ordered list book ids
 
 # Not tested
-def save_submission(individuals):
+def save_submission(L, books):
     # Assume tuple of library list and book id
-    libraries, book_dict = individuals
-    no_signed_libs = len(libraries)
+    no_signed_libs = len(L)
 
     # Open a file with access mode 'a'
     with open("submission.txt", "a") as file_object:
         file_object.write(str(no_signed_libs) + '\n')
-        for lib_id in libraries:
-            books = book_dict[lib_id]
+        for lib_id in L:
+            books = books[lib_id]
             no_books_scanned = len(books)
             book_string = " ".join(str(x) for x in books)
             file_object.write(str(lib_id) + str(no_books_scanned) + '\n')
