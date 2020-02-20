f3dfba32cb9cdb2dfc7277b9b2d963819e223661
Maurice Frank <maurice.frank@posteo.de>
2020-02-20 21:56:21 +0100

    changed  fitn

--- fitness.py
+++ fitness.py
@@ -1,31 +1,25 @@
 from collections import defaultdict
 
-# Do this at beginning of new training
-def setup_active_library_list(L, D, L_signuptimes):
-    # Contains list of active libraries on that day (filling up)
-    active_libs = [[] for _ in range(D)]
-    i = 0
-    for l in L:
-        i += L_signuptimes[l]
-        if i >= D:
-            break
-        for j in range(i, D):
-            active_libs[j].append(l)
-    return active_libs
 
-
-# Optimization function, active_libs comes from above
 def fitness(L, books, D, B_scores, L_signuptimes, L_shipperday):
     assert len(books) == len(L)
     scanned_books = set()
 
-    active_libs = setup_active_library_list(L, D, L_signuptimes)
-
     i_L = defaultdict(int)
 
     score = 0.
+    waiting = False
+    setupp = 0
+    active_libs = []
+    il = 0
     for d in range(D):
-        for lib in active_libs[d]:
+        if not waiting:
+            if il < len(L):
+                weiting_l = L[il]
+                setupp = L_signuptimes[weiting_l]
+                il += 1
+                waiting = True
+        for lib in active_libs:
             for _ in range(L_shipperday[lib]):
                 if i_L[lib] < len(books[lib]) - 1:
                     book = books[lib][i_L[lib]]
@@ -33,6 +27,12 @@ def fitness(L, books, D, B_scores, L_signuptimes, L_shipperday):
                         scanned_books.add(book)
                         score += B_scores[book]
                     i_L[lib] += 1
+        if waiting:
+            setupp -= 1
+            if setupp == 0:
+                active_libs.append(weiting_l)
+                waiting = False
+
     return score
 
 
